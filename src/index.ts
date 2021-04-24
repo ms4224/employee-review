72ab0976127cbc4a0c83d2e944dffa523e5684ae
Matias <schakelmatias@gmail.com>
2021-04-24 14:28:27 +0900

    Fixed port for heroku

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'static')));
 
