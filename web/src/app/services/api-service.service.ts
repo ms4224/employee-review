3c977c663f7b6576a0dbc081d2e8b7d52b7c3640
Matias <schakelmatias@gmail.com>
2021-04-26 20:26:07 +0900

    added api service files

--- web/src/app/services/api-service.service.ts
+++ web/src/app/services/api-service.service.ts
@@ -8,12 +8,43 @@ import { map } from 'rxjs/operators';
 })
 export class ApiService {
   private readonly employeeBaseUrl = 'api/employee';
+  private readonly reviewBaseUrl = 'api/review';
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any>  {
-    return this.http.get(this.employeeBaseUrl).pipe(
-      map(res => res)
-    )
+  public getEmployees(): Observable<IEmployee[]>  {
+    return this.http.get<IEmployee[]>(this.employeeBaseUrl)
+  }
+
+  public addEmployee(firstName: string, lastName: string): Observable<void> {
+    return this.http.post<void>(this.employeeBaseUrl, {firstName: firstName, lastName: lastName})
+  }
+
+  public deleteEmployee(lastName: string): Observable<void> {
+    return this.http.delete<void>(`${this.employeeBaseUrl}/${lastName}`);
+  }
+
+  public updateEmployee(lastName: string, newFirstName: string, newLastName: string): Observable<void> {
+    return this.http.put<void>(`${this.employeeBaseUrl}/${lastName}`, {newFirstName: newFirstName, newLastName: newLastName});
+  }
+
+  public getReviews(): Observable<IReview[]>  {
+    return this.http.get<IReview[]>(this.reviewBaseUrl)
+  }
+
+  public addReview(title: string): Observable<void> {
+    return this.http.post<void>(this.reviewBaseUrl, {title: title})
+  }
+
+  public updateReview(title: string, newTitle: string): Observable<void> {
+    return this.http.put<void>(`${this.reviewBaseUrl}/${title}`, {newTitle: newTitle});
+  }
+
+  public assignNewFeedback(title: string, reviewee: string, reviewer: string): Observable<void> {
+    return this.http.post<void>(`${this.reviewBaseUrl}/${title}`, {reviewee: reviewee, reviewer: reviewer});
+  }
+
+  public updateFeedback(title: string, reviewee: string, reviewer: string, feedback: string): Observable<void> {
+    return this.http.put<void>(`${this.reviewBaseUrl}/${title}/${reviewee}/${reviewer}`, {feedback: feedback});
   }
 }
--- web/src/app/web.d.ts
+++ web/src/app/web.d.ts
@@ -2,3 +2,14 @@ type IEmployee = {
     firstName: string;
     lastName: string;
 }
+
+type IReview = {
+    title: string;
+}
+
+type IFeedback = {
+    reviewee: string;
+    reviewer: string;
+    title: string;
+    feedback: string;
+}
